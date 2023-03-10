effe9ecdb506c6cfec31f26b5b1bd4034bd354ca
NataliiaSav <savchenko6561@gmail.com>
2023-03-10 12:26:34 +0200

    update

---
+++ src/js/task-01.js
@@ -0,0 +1,4 @@
+const categoriesListEl = document.querySelector("ul#categories");
+const categoriesItemEl = document.querySelectorAll("li.item");
+console.log(categoriesItemEl);
+console.log(`Number of categories: ${categoriesItemEl.length}`);
