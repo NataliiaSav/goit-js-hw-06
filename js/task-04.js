8542f36855664451895a2582e6664f9ae396c47a
NataliiaSav <savchenko6561@gmail.com>
2023-03-12 11:28:35 +0200

    add task 4

---
+++ js/task-04.js
@@ -0,0 +1,14 @@
+let counterValue = 0;
+const decrementBtn = document.querySelector('[data-action="decrement"]');
+const incrementBtn = document.querySelector('[data-action="increment"]');
+const valueEl = document.querySelector("#value");
+const decrementClick = (event) => {
+  counterValue += 1;
+  valueEl.textContent = counterValue;
+};
+const incrementClick = (event) => {
+  counterValue -= 1;
+  valueEl.textContent = counterValue;
+};
+decrementBtn.addEventListener("click", decrementClick);
+incrementBtn.addEventListener("click", incrementClick);
