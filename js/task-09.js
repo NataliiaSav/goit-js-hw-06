5699c12caa9cd54bbc09455b19ef4cc93c05ea7d
NataliiaSav <savchenko6561@gmail.com>
2023-03-14 19:48:35 +0200

    add task 9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,11 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const btnChangeColor = document.querySelector(".change-color");
+const spanTextColor = document.querySelector(".color");
+const bodyEl = document.body;
+function handlerButtonCliсk(event) {
+  bodyEl.style.backgroundColor = spanTextColor.textContent =
+    getRandomHexColor();
+}
+btnChangeColor.addEventListener("click", handlerButtonCliсk);
