8cd043f234afe492bd1e648801a1e4a613ed7da1
NataliiaSav <savchenko6561@gmail.com>
2023-03-14 16:41:32 +0200

    add task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,13 @@
-
+const formEl = document.querySelector(".login-form");
+formEl.addEventListener("submit", handlerSubmit);
+function handlerSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+  if (email.value === "" || password.value === "") {
+    alert("Поле має бути заповнене");
+  }
+  console.log(`E-mail: ${email.value}, Password: ${password.value}`);
+  event.currentTarget.reset();
+}
